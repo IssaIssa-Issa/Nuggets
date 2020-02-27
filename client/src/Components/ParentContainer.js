428d1c6e8e967c7218afd2778f89a20469e7d09a
Elizkoleary <eliz.k.oleary@gmail.com>
2020-02-26 21:23:51 -0600

    add chores button

--- client/src/Components/ParentContainer.js
+++ client/src/Components/ParentContainer.js
@@ -3,19 +3,36 @@ import KidCard from './KidCard'
 import PendingTranList from './PendingTranList'
 import AddChildBtn from './addChildBtn'
 import AddFundsBtn from './addFundsBtn'
+import AddChoresBtn from './addChoresBtn'
 
 const ParentContainer = () => {
-return(
-<div>
-    <AddChildBtn />
-    <AddFundsBtn />
-    <KidCard /> 
-    <br></br>
-    <br></br>
-    <br></br>
-    <PendingTranList />
-</div>
-)
+    return (
+        <div>
+            <div className="row">
+                <div className="col-md-3"></div>
 
-}
+                <div className="col-md-2">
+                    <AddChildBtn />
+                </div>
+                <div className="col-md-2">
+                    <AddFundsBtn />
+                </div>
+                <div className="col-md-2">
+                    <AddChoresBtn />
+                </div>
+                <div className="col-md-3"></div>
+
+                </div>
+                <br></br>
+                <br></br>
+
+                <KidCard />
+                <br></br>
+                <br></br>
+                <br></br>
+                <PendingTranList />
+            </div>
+            )
+            
+            }
 export default ParentContainer
---
+++ client/src/Components/addChoresBtn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddChoresBtn = () => {
+    return(
+<button type="button" class="btn btn-primary">Add Chores</button>
+
+
+    )};
+    export default AddChoresBtn
