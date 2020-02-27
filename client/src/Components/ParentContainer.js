f4c45c9bb9f57865295cf4b64b14a577b156b663
Elizkoleary <eliz.k.oleary@gmail.com>
2020-02-26 21:09:06 -0600

    add funds button

--- client/src/Components/ParentContainer.js
+++ client/src/Components/ParentContainer.js
@@ -2,11 +2,13 @@ import React from 'react'
 import KidCard from './KidCard'
 import PendingTranList from './PendingTranList'
 import AddChildBtn from './addChildBtn'
+import AddFundsBtn from './addFundsBtn'
 
 const ParentContainer = () => {
 return(
 <div>
     <AddChildBtn />
+    <AddFundsBtn />
     <KidCard /> 
     <br></br>
     <br></br>
---
+++ client/src/Components/addFundsBtn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddFundsBtn = () => {
+    return(
+<button type="button" class="btn btn-primary">Add Funds</button>
+
+
+    )};
+    export default AddFundsBtn
