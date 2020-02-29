9dcacc19c025ce64449829d57dd5a240a8cce430
Kayla Kuhlman <kakoreo4@gmail.com>
2020-02-29 13:47:07 -0600

    added functionality to managechorebtn

--- client/src/Components/Buttons/manageChoresBtn.js
+++ client/src/Components/Buttons/manageChoresBtn.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 
 const ManageChoresBtn = () => {
+
+        const history = useHistory();
+      
+        function handleClick() {
+          history.push("/managechoresform");
+        }
+      
     return(
-<button type="button" class="btn btn-primary">Manage Chores</button>
+        
+<button type="button" onClick={handleClick} class="btn btn-primary">Manage Chores</button>
 
 
     )};
