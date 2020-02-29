5f88c2d9fce7c08683914b29ab692e140bd46501
Kayla Kuhlman <kakoreo4@gmail.com>
2020-02-29 13:25:09 -0600

    added functionality to managefundsbtn

--- client/src/Components/Buttons/manageFundsBtn.js
+++ client/src/Components/Buttons/manageFundsBtn.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 
 const ManageFundsBtn = () => {
+
+    routeChange=()=> {
+        let path = `/managefundsform`;
+        let history = useHistory();
+        history.push(path);
+      }
+
     return(
-<button type="button" class="btn btn-primary">Manage Funds</button>
+<button type="button" onClick={this.routeChange} class="btn btn-primary">Manage Funds</button>
 
 
     )};
