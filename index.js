3769b182fbdd4614ecc0f048297fb81071ea0e5a
Kayla Kuhlman <kakoreo4@gmail.com>
2020-03-11 17:14:47 -0500

    updated idnex.js to include static public folder

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.use((err, req, res, next) => {
 app.use(apiRoute)
 
 app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/public')));
+
 
 app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
