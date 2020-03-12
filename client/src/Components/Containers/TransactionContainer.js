97992096ad5099337624722cca7fbc36ac41aa0e
Elizkoleary <eliz.k.oleary@gmail.com>
2020-03-12 10:44:48 -0500

    added transactioncontainer

---
+++ client/src/Components/Containers/TransactionContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Transactions from './Transactions'
+
+const TransactionContainer = () => {
+    return(
+        <div className="childContainer">
+        <Transactions />
+        </div>
+    )
+}
+
+export default TransactionContainer;
