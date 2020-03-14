137ce5b7ed2eb3d308d39509483e20fd74eee3bd
Kayla Kuhlman <kakoreo4@gmail.com>
2020-03-14 09:08:06 -0500

    updated config file for heroku

--- models/index.js
+++ models/index.js
@@ -10,16 +10,12 @@ const db = {};
 
 let sequelize;
 
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL, {
-//   });
-// } else {
+let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-// }
 
 fs
   .readdirSync(__dirname)
