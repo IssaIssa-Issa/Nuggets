c875913d2d534683be907ebd298264d0f45d492b
Kayla Kuhlman <kakoreo4@gmail.com>
2020-03-15 23:35:33 -0500

    added JAWS_URL to env in index.js

--- models/index.js
+++ models/index.js
@@ -5,10 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.js')[env];
+var config = require(__dirname + '/../config/config.js')[env];
 const db = {};
-
-require('dotenv').config()
 
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
