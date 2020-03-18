723a19c38028c57fff0df57c18843e6d3a28f8b5
Kayla Kuhlman <kakoreo4@gmail.com>
2020-03-17 20:23:55 -0500

    added middleware to handle proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,  { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import login from './Components/Containers/login';
