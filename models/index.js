48b0dce56715f1cf95cdc08487da7ab980660297
Kayla Kuhlman <kakoreo4@gmail.com>
2020-03-17 22:40:38 -0500

    reverted index.js

--- models/index.js
+++ models/index.js
@@ -1,18 +1,36 @@
+
 'use strict';
 
 const fs = require('fs');
 const path = require('path');
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-var config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+
+let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL, {
+
+//     //Find a way to hide credentials when logging into remote jaws db with sequelize package
+//     dialect: "mysql",
+//     protocol: "mysql",
+//     port: 3306,
+//     host: "<heroku host>",
+//     logging: true //false
+//   });
+
+// } else {
+
 
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+// }
 
 fs
   .readdirSync(__dirname)
@@ -31,6 +49,5 @@ Object.keys(db).forEach(modelName => {
 });
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
 module.exports = db;
