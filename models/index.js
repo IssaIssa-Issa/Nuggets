08fdd4df8734c87fdd3f329ddd960493104669e4
Kayla Kuhlman <kakoreo4@gmail.com>
2020-03-17 22:26:41 -0500

    added middleware to handle proxy

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/../config/config.json')[env];
